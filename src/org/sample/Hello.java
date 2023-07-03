a243ff204193f2dfeb5fa0e249b18f6fb167e404
Sabasiva1629 <sabasiva1629@gmail.com>
2023-07-03 09:51:30 +0530

    Reverse of String

--- src/org/sample/Hello.java
+++ src/org/sample/Hello.java
@@ -5,6 +5,17 @@ public class Hello {
     public static void main(String[] args) {
 
 	System.out.println("Hello World");
+	String str = "java programm";
+	// Doing Reverse of given String
+	String[] split = str.split(" ");
+	String output = "";
+	for (String string : split) {
+	    for (int i = string.length() - 1; i >= 0; i--) {
+		output = output + string.charAt(i);
+	    }
+	    output = output + " ";
+	}
+	System.out.println("Reverse of given String :" + output);
     }
 
 }
